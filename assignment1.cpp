1dab1b199d736a90b673f299de628209b6abd6e6
VIDHI ANAND <VIDHIANAND0211@GMAIL.COM>
2023-02-23 21:54:10 +0530

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first, second, sum;
+    
+  cout << "Enter two integers: ";
+  cin >> first >> second;    
+
+  return 0;
+}
---
+++ assignment2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first, second, sum;
+    
+  cout << "Enter two integers: ";
+  cin >> first >> second;  
+
+  if (first > second)  {
+    cout<<"First is the greator number";
+  }
+
+  else{
+    cout<<"second is the greator number";
+  }
+
+  return 0;
+}
