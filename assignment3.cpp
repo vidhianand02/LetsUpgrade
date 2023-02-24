388507ac96ab084877880cb78e2ed399ef6997d7
VIDHI ANAND <VIDHIANAND0211@GMAIL.COM>
2023-02-24 20:23:33 +0530

    Add files via upload

---
+++ assignment3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int i,n;
+    
+  cout << "Enter an integer: ";
+  cin >> n;  
+
+ for ( i = 0; i <=n; i++)
+ {
+    if (i%2==0)
+    {
+        cout<<i<<endl;
+    }
+   
+ }
+ 
+  return 0;
+}
